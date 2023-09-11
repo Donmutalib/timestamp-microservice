7d31f174f8771073c313601e8269b26033472cb3
donmutalib <backendkolawole@gmail.com>
2023-09-11 08:49:48 +0100

    second commit, removed public and static files

--- index.js
+++ index.js
@@ -1,65 +1,35 @@
-// index.js
-// where your node app starts
-
-// init project
-var express = require('express');
-var app = express();
 require('dotenv').config()
+const express = require('express');
+const app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
-});
+const cors = require('cors');
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  console.log(req.header)
-  res.json({greeting: 'hello API'});
-});
+app.get('/api/:date?', (req, res) => {
 
-
-
-
-// your second API endpoint... 
-
-app.get("/api/:date", function(req, res){
-  let dateString = req.params.date
-
-
-  if (/\d{5,}/.test(dateString)) {
-    dateInt = parseInt(dateString);
-    res.json({ unix: dateInt, utc: new Date(dateInt).toUTCString() });
+  let { date } = req.params
+  if (!date) {
+    return res.json({ unix: Number(new Date()), utc: new Date().toUTCString() })
   }
 
-  let dateObject = new Date(dateString);
-
-  if (dateObject.toString() === "Invalid Date") {
-    res.json({ error: "Invalid Date" });
-  } else {
-    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+  if (+date) {
+    return res.send({ unix: Number(date), utc: new Date(Number(date)).toUTCString() })
   }
-  
+
+  date = new Date(date)
+
+  if (date == 'Invalid Date') {
+    return res.send({ error: "Invalid Date" })
+  }
+  res.json({ unix: Number(date), utc: date.toUTCString() })
 })
 
-app.get("/api/", function (req, res) {
-  let date = new Date()
-
-  res.json({ unix: date.valueOf(), utc: date.toUTCString() });
-  
-});
 
 
+const port = process.env.PORT
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(port, function () {
+  console.log('Your app is listening on port ' + port);
 });
